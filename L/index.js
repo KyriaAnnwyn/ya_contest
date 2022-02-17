4923490a7089f84e7c76a56d9918c092a13728d2
KyriaAnnwyn <36903488+KyriaAnnwyn@users.noreply.github.com>
2022-02-17 12:24:36 +0300

    Update index.js

--- L/index.js
+++ L/index.js
@@ -1,14 +1,19 @@
 module.exports = function (vector) {
   let result = 0;
-  let currMax = 0;
+  let currMaxPrev = 0;
+  let currMaxCur = 0;
   const TARGET_NUM = "1";
   for (let i = 0; i < vector.length; i++) {
     if (vector[i] === TARGET_NUM) {
-      currMax++;
-      if (currMax > result) result = currMax;
+      currMaxCur++;
+      if (currMaxCur + currMaxPrev > result) result = currMaxCur + currMaxPrev;
     } else {
-      currMax = 0;
+      currMaxPrev = currMaxCur;
+      currMaxCur = 0;
     }
   }
+  
+  if (result === vector.length) result--;
+  
   return result;
 };
